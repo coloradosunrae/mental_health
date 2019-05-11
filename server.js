ba190453d7c19330373b9c495ef33ac5ec80188d
Coloradosunrae <stevie@ourhopecenter.org>
2019-05-10 23:11:41 -0600

    heroku

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ var bodyParser = require('body-parser');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
+
 // Add routes, both API and view
 app.use(routes);
 
